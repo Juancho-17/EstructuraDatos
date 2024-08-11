9ef85a02d749a9f9c195501d16f5dd47bba2e060
Juan jose tique <juan.tique@estudiantesunibague.edu.co>
2024-08-10 22:30:07 -0500

    añadir variable

--- src/estructuradatos/EstructuraDatos.java
+++ src/estructuradatos/EstructuraDatos.java
@@ -14,7 +14,7 @@ public class EstructuraDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         int estudiantes;
     }
     
 }
